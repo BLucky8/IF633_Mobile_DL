b40eeda574c776f629004c3655be0f58bc011143
BLucky8 <lucky.benedict@student.umn.ac.id>
2021-02-22 19:27:40 +0700

    Week 04 B

---
+++ week4_31943_B/app/src/main/java/com/example/week4_31943_b/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.week4_31943_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button halaman1, halaman2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        halaman1 = findViewById(R.id.main_button_change_1);
+        halaman2 = findViewById(R.id.main_button_change_2);
+
+        halaman1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        halaman2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ week4_31943_B/app/src/main/java/com/example/week4_31943_b/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.week4_31943_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
